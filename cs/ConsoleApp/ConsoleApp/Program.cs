9cc3cfe8af2ec0a0589986725a395ee0ede96c58
Psilon <pzixel@gmail.com>
2018-06-15 17:00:59 +0300

    vector from rust

--- cs/ConsoleApp/ConsoleApp/Program.cs
+++ cs/ConsoleApp/ConsoleApp/Program.cs
@@ -12,20 +12,28 @@ namespace ConsoleApp
         {
             var result = RustInterop.Println("Hello", "World");
             Console.WriteLine($"Result was {result}");
+
+            var range = RustInterop.GetRange(10);
+            Console.WriteLine(string.Join("\n", range));
         }
     }
 
     public class RustInterop
     {
         [StructLayout(LayoutKind.Sequential)]
-        struct StringRef
+        struct ArrayRef
         {
             public IntPtr Bytes;
             public uint Length;
         }
 
+        private delegate void GetRangeCallback(ArrayRef array);
+
         [DllImport("rust.dll")]
-        private static extern int println(StringRef[] array, uint count);
+        private static extern int println(ArrayRef[] array, uint count);
+
+        [DllImport("rust.dll")]
+        private static extern int get_range(uint count, GetRangeCallback callback);
 
         public static int Println(params string[] strings)
         {
@@ -33,7 +41,7 @@ namespace ConsoleApp
             {
                 var pointer = Marshal.AllocHGlobal(bytes.Length);
                 Marshal.Copy(bytes, 0, pointer, bytes.Length);
-                return new StringRef
+                return new ArrayRef
                 {
                     Bytes = pointer,
                     Length = (uint) bytes.Length
@@ -45,6 +53,18 @@ namespace ConsoleApp
                 Marshal.FreeHGlobal(stringRef.Bytes);
             }
             return i;
+        }
+
+        public static int[] GetRange(uint count)
+        {
+            int[] result = null;
+            get_range(count, arrayRef =>
+            {
+                var res = new int[arrayRef.Length];
+                Marshal.Copy(arrayRef.Bytes, res, 0, res.Length);
+                result = res;
+            });
+            return result;
         }
     }
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,11 +1,11 @@
 #[repr(C)]
-pub struct StringRef {
-    bytes: *const u8,
+pub struct ArrayRef<T> {
+    bytes: *const T,
     length: usize
 }
 
 #[no_mangle]
-pub extern fn println(array: *const StringRef, count: usize) -> i32 {
+pub extern fn println(array: *const ArrayRef<u8>, count: usize) -> i32 {
     let array_slice = unsafe { std::slice::from_raw_parts(array, count) };
     for str in array_slice {
         let slice = unsafe { std::slice::from_raw_parts(str.bytes, str.length) };
@@ -18,4 +18,14 @@ pub extern fn println(array: *const StringRef, count: usize) -> i32 {
         };
     }
     1
+}
+
+#[no_mangle]
+pub extern fn get_range(count: usize, callback: extern fn(ArrayRef<i32>)) {
+    let range: Vec<_> = (1..count).map(|x| x as i32).collect();
+    let array_ref = ArrayRef {
+        bytes: range.as_ptr(),
+        length: range.len()
+    };
+    callback(array_ref)
 }
