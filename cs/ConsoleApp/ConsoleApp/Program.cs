d8287dd220682f5dec98538b7c13757527da2302
Psilon <pzixel@gmail.com>
2018-06-15 15:45:33 +0300

    array of strings

--- cs/ConsoleApp/ConsoleApp/Program.cs
+++ cs/ConsoleApp/ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -9,21 +10,41 @@ namespace ConsoleApp
 
         static void Main(string[] args)
         {
-            var result = RustInterop.Println("Hello World!");
+            var result = RustInterop.Println("Hello", "World");
             Console.WriteLine($"Result was {result}");
         }
     }
 
     public class RustInterop
     {
+        [StructLayout(LayoutKind.Sequential)]
+        struct StringRef
+        {
+            public IntPtr Bytes;
+            public uint Length;
+        }
 
         [DllImport("rust.dll")]
-        private static extern int println(byte[] bytes, uint count);
+        private static extern int println(StringRef[] array, uint count);
 
-        public static int Println(string str)
+        public static int Println(params string[] strings)
         {
-            var bytes = Encoding.UTF8.GetBytes(str);
-            return println(bytes, (uint) bytes.Length);
+            var array = strings.Select(str => Encoding.UTF8.GetBytes(str)).Select(bytes =>
+            {
+                var pointer = Marshal.AllocHGlobal(bytes.Length);
+                Marshal.Copy(bytes, 0, pointer, bytes.Length);
+                return new StringRef
+                {
+                    Bytes = pointer,
+                    Length = (uint) bytes.Length
+                };
+            }).ToArray();
+            int i = println(array, (uint) array.Length);
+            foreach (var stringRef in array)
+            {
+                Marshal.FreeHGlobal(stringRef.Bytes);
+            }
+            return i;
         }
     }
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,13 +1,21 @@
-//Rust code
+#[repr(C)]
+pub struct StringRef {
+    bytes: *const u8,
+    length: usize
+}
+
 #[no_mangle]
-pub extern fn println(bytes: *const u8, count: usize) -> i32 {
-    let slice = unsafe { std::slice::from_raw_parts(bytes, count) };
-    let str = std::str::from_utf8(slice);
-    match str {
-        Ok(str) => {
-            println!("{}", str);
-            1
-        },
-        Err(_) => -1
+pub extern fn println(array: *const StringRef, count: usize) -> i32 {
+    let array_slice = unsafe { std::slice::from_raw_parts(array, count) };
+    for str in array_slice {
+        let slice = unsafe { std::slice::from_raw_parts(str.bytes, str.length) };
+        let str = std::str::from_utf8(slice);
+        match str {
+            Ok(str) => {
+                println!("{}", str);
+            },
+            Err(_) => return -1
+        };
     }
+    1
 }
